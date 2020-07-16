f63b5724856b31cab795e16e9820d4152665fba1
Hiren Mistry <hirenmst@gmail.com>
2020-07-16 03:51:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,13 +3,13 @@ def bonus
   epic_tragedy = {
    :montague => {
      ....
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", status: "dead"},
     ...
       ]
    },
    :capulet => {
       ...
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
     ...
       ]
    }
