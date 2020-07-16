c92a6ea45562b111f6540eb8f39d9636a7507b7c
Hiren Mistry <hirenmst@gmail.com>
2020-07-16 03:58:06 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,4 +1,25 @@
 def bonus
+  def bonus	epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+  epic_tragedy = {	
+   :montague => {	puts epic_tragedy
+      :patriarch => {name: "Lord Montague", age: "53"},	
+      :matriarch => {name: "Lady Montague", age: "54"},	#  =>
+      :hero => {name: "Romeo", age: "15", status: "alive"},	{
+      :hero_friends => [	   :montague => {
+         {name: "Benvolio", age: "17", attitude: "worried"},	      :patriarch => {name: "Michael Jordan", age: "53"},
+         {name: "Steven", age: "30", attitude: "confused"}	      :matriarch => {name: "Lady Montague", age: "54"},
+      ]	      :hero => {name: "Romeo", age: "15", status: "alive"},
+   }, 	      :hero_friends => [
+   :capulet => {	        {name: "Benvolio", age: "17", attitude: "worried"},
+      :patriarch => {name: "Lord Capulet", age: "50"},	        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      :matriarch => {name: "Lady Capulet", age: "51"},	      ]
+      :heroine => {name: "Juliet", age: "15", status: "alive"},	   },
+      :heroine_friends => [	   :capulet => {
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 	      :patriarch => {name: "Lord Capulet", age: "50"},
+          {name: "Nurse", age: "44", attitude: "worried"}	      :matriarch => {name: "Lady Capulet", age: "51"},
+      ]	      :heroine => {name: "Juliet", age: "15", status: "alive"},
+   }	      :heroine_friends => [
+  }
    epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
   }
